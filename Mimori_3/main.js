34d1c91df62926a5e50265070e49f106d34d87d9
cogcamp <codeofgenius.camp@gmail.com>
2023-07-28 06:42:08 +0000

    Mimori_3

--- Mimori_3/main.js
+++ Mimori_3/main.js
@@ -2,37 +2,62 @@ var mainScene = new Phaser.Scene("mainScene");
 
 mainScene.create = function() {
     // 初期設定を実行する
-    
+    this.config();
     
     // ボール作成
-    
+    this.createBall();
     
     // パドル作成
-    
+    this.createPaddle();
     
     // スペースキーのクリックでボール発射
-    
+    this.input.keyboard.on('keydown-SPACE',function(event){
+        //ゲーム開始状態ならば
+        if(this.paddle.isStart){
+            //ボール発射
+            this.ball.setVelocity(this.ballSpeedX,this.ballSpeedY);
+            this.paddle.isStart = false;
+        }
+    },this);
     
     // ブロック作成
-    
+    this.createBlocks();
     
     // ライフのテキスト表示
-    
+    this.lifeText = this.add.text(30, 20, 'ライフ:' + this.life,{
+        font: '20px Open Sans',
+        fill: '#ff0000'
+    });
 };
 
 mainScene.update = function() {
     // ボールがシーンの最下部に到達した
-    
+    if(this.ball.y >= this.game.config.height - this.ball.width / 2){
+        this.failToHit();
+    }
     
     // キーボードのカーソルオブジェクトを取得
     var cursors = this.input.keyboard.createCursorKeys();
     var x = 0;
-    
+    //右カーソルをクリックすると
+    if(cursors.right.isDown){
+        x = this.paddle.x + this.paddleSpeed;
+        this.paddle.x = Phaser.Math.Clamp(x, 52, 748);
+    }
+    //左カーソルをクリックすると
+    if(cursors.left.isDown){
+        x = this.paddle.x - this. paddleSpeed;
+        this.paddle.x = Phaser.Math.Clamp(x, 52, 748);
+    }
+    //パドルの上にボールが乗っているなら
+    if(this.paddle.isStart){
+        this.ball.setPosition(this.paddle.x,500);
+    }
 };
 
 mainScene.config = function() {
     // 背景色の設定
-    this.cameras.main.setBackgroundColor('#cccccc');
+    this.cameras.main.setBackgroundColor('#7CA1F0');
     
     // パドルの移動速度
     this.paddleSpeed = 10;
@@ -42,28 +67,72 @@ mainScene.config = function() {
     this.ballSpeedY = -300;
     
     // ライフ
-    this.life = 3;
+    this.life = 6;
 };
 
 mainScene.createBall = function() {
     // ボール作成
-    
+    this.ball = this.physics.add.image(400,500,'ball2');
+    this.ball.setDisplaySize(22,22);
+    this.ball.setCollideWorldBounds(true);
+    this.ball.setBounce(1);
 };
 
 mainScene.createPaddle = function() {
      // パドル作成
-    
+    this.paddle = this.physics.add.image(400,550,'paddle1');
+    this.paddle.setDisplaySize(104,24);
+    this.paddle.setImmovable();
+    this.paddle.isStart = true;
+    this.physics.add.collider(this.paddle,this.ball,this.hitPaddle,null,this);
 };
 
 mainScene.hitPaddle = function (paddle, ball) {
     // ボールにX方向の角度を設定
-    
+    var diff = 0;
+    if(ball.x < paddle.x){
+        //ボールがパドルの左側に衝突
+        diff = paddle.x - ball.x;
+        ball.setVelocityX(-10 * diff);
+    }else if (ball.x -paddle.x){
+        // ボールがパドルの右側に衝突
+        diff = ball.x -paddle.x;
+        ball.setVelocityX(10 * diff);
+    } else {
+        // X方向の加速度はなし
+        ball.setVelocityX(0);
+    }
 };
 
 mainScene.createBlocks = function() {
-    // 横10列、縦6行並べる
-    
-    
+    // 横10列、縦8行並べる
+    // ブロックの色の配列
+    var blockColors = [ 'yellow1','yellow2','purple1','purple2','blue1','blue2','green1','green2'];
+    // 物理エンジン対象固定オブジェクトグループ作成
+    this.blocks = this.physics.add.staticGroup();
+    //縦に8行
+    for( var i = 0; i < 8; i++){
+        //横に10列
+        for( var j = 0; j < 12; j++){
+            var color;
+            if (i == 0 && j == 5) {
+                color = "silver1";
+            } else if (j==4){
+                color = "silver1";
+            } else if (j == 7){
+                color = 'silver1';
+            } else if (i == 0 && j == 6) {
+                color = 'silver1';
+            } else{
+            color = blockColors[i];
+            }
+            var block = this.blocks.create(20 + j * 64, 80 + i * 32,color);
+            block.setOrigin(0,0);
+            block.setDisplaySize(64,32);
+            block.refreshBody();
+        }
+    }
+    this.physics.add.collider(this.ball, this.blocks, this.hitBlock, null, this);
 };
 
 mainScene.hitBlock = function (ball, block) {
