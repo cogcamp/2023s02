405be037a0d64d3d15f091569957e3d2c785e50a
cogcamp <codeofgenius.camp@gmail.com>
2023-08-04 06:47:01 +0000

    Akira_4

--- Akira_4/main.js
+++ Akira_4/main.js
@@ -2,32 +2,67 @@ var mainScene = new Phaser.Scene("mainScene");
 
 mainScene.create = function() {
     // 初期設定を実行する
-    
+this.config();
     
     // ボール作成
-    
+this.createBall();   
     
     // パドル作成
-    
+this.createPaddle();    
     
     // スペースキーのクリックでボール発射
-    
+    this.input.keyboard.on("keydown-SPACE",function(event){
+        //ゲーム開始状態ならば
+        if(this.paddle.isStart){
+            //ボール発射
+            this.ball.setVelocity(this.ballSpeedX, this.ballSpeedY);
+            this.paddle.isStart = false;
+        }
+    }, this);
     
     // ブロック作成
-    
+    this.createBlocks();
     
     // ライフのテキスト表示
+    this.lifeText = this.add.text(30,20,"ライフ:"+ this.life,{
+        font: "20px Open Sans",
+        fill: "#ff0000"
+    });
+    
+    this.timeEvent = this.time.addEvent({
+        delay: 1500,
+        callback: this.changeColor,
+        loop: true,
+        callbackScope: this
+    });
     
 };
 
 mainScene.update = function() {
     // ボールがシーンの最下部に到達した
-    
+    if (this.ball.y >= this.game.config.height - this.ball.width / 2 ){
+        this.failToHit();
+    }
     
     // キーボードのカーソルオブジェクトを取得
     var cursors = this.input.keyboard.createCursorKeys();
     var x = 0;
-    
+    // 右カーソルをクリックすると
+    if(cursors.right.isDown){
+        x = this.paddle.x + this.paddleSpeed;
+        this.paddle.x = Phaser.Math.Clamp(x,52,748);
+    }
+    //左カーソルをクリックすると
+    if(cursors.left.isDown){
+        x = this.paddle.x - this.paddleSpeed;
+        this.paddle.x = Phaser.Math.Clamp(x,52,748);
+    }
+
+
+    //パドルの上にボールが乗っているなら
+    if(this.paddle.isStart){
+        this.ball.setPosition(this.paddle.x,500);
+    }
 };
 
 mainScene.config = function() {
@@ -35,35 +70,121 @@ mainScene.config = function() {
     this.cameras.main.setBackgroundColor('#cccccc');
     
     // パドルの移動速度
-    this.paddleSpeed = 10;
+    this.paddleSpeed = 11;
     
     // ボール発射の加速度
     this.ballSpeedX = 0;
     this.ballSpeedY = -300;
     
     // ライフ
-    this.life = 3;
+    this.life = 4;
 };
+
+mainScene.changeColor = function() {
+    // 背景色を変更
+    var randomNumber = Phaser.Math.RND.between(1, 4);
+    if (randomNumber == 1) {
+        this.cameras.main.setBackgroundColor('#135791');
+        
+    } else if (randomNumber == 2) {
+        this.cameras.main.setBackgroundColor('#00ff00');
+        
+    } else if (randomNumber == 3) {
+        this.cameras.main.setBackgroundColor('#fff000');
+        
+    } else if (randomNumber == 4) {
+        this.cameras.main.setBackgroundColor('#0000ff');
+}
+
+    };
+
 
 mainScene.createBall = function() {
     // ボール作成
-    
+this.ball = this.physics.add.image(400,500,"ball1");
+this.ball.setDisplaySize(22,22);
+this.ball.setCollideWorldBounds(true);
+this.ball.setBounce(1);
 };
 
 mainScene.createPaddle = function() {
      // パドル作成
-    
+this.paddle = this.physics.add.image(400,550,"paddle1");
+this.paddle.setDisplaySize(104,24);
+this.paddle.setImmovable();
+this.paddle.isStart = true;
+this.physics.add.collider(this.paddle, this.ball, this.hitPaddle,null,this);
 };
 
 mainScene.hitPaddle = function (paddle, ball) {
     // ボールにX方向の角度を設定
-    
+    var diff = 0;
+    if (ball.x < paddle.x){
+        //ボールがパドルの左側に衝突
+        diff = paddle.x - ball.x;
+        ball.setVelocityX(-10 * diff);
+    }else if (ball.x > paddle.x){
+        //ボールがパドルの右側に衝突
+        diff = ball.x -paddle.x;
+        ball.setVelocityX(10 * diff);
+    }else {
+        //x方向の加速度はなし
+        ball.setVelocityX(0);
+    }
 };
 
 mainScene.createBlocks = function() {
     // 横10列、縦6行並べる
+    //ブロックの色の配列
+    var blockColors = ["silver2","silver2","silver2","silver2","silver2","silver2","silver2","silver2"];
     
+    //物理エンジン対象固定オブジェクトグループ作成
+    this.blocks = this.physics.add.staticGroup();
     
+    //縦に8行
+    for(var i = 0; i < 8; i++){
+        //横に9列
+        for( var j = 0; j < 9; j++){
+            var color = blockColors[i];
+            
+            
+            
+              if (j==3 && i==2) {
+                color = 'red1';
+              
+            }else if (j==3 && i==3) {
+                color = 'red1';
+            }else if (j==3 && i==4) {
+                color = 'red1';
+            }else if (j==3 && i==5) {
+                color = 'red1';
+            }else if (j==4 && i==2) {
+                color = 'red1';
+            }else if (j==4 && i==3) {
+                color = 'red1';
+            }else if (j==4 && i==4) {
+                color = 'red1';
+            }else if (j==4 && i==5) {
+                color = 'red1';
+            }else if (j==5 && i==2) {
+                color = 'red1';
+            }else if (j==5 && i==3) {
+                color = 'red1';
+            }else if (j==5 && i==4) {
+                color = 'red1';
+            }else if (j==5 && i==5) {
+                color = 'red1';
+           
+            }
+    
+            var block = this.blocks.create(80 + j * 64, 80 + i * 32, color);
+            block.setOrigin(0,0);
+            block.setDisplaySize(64,32);
+            block.refreshBody();
+        }
+    }
+    
+    this.physics.add.collider(this.ball, this.blocks, this.hitBlock, null, this);
 };
 
 mainScene.hitBlock = function (ball, block) {
