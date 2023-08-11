ff4f21d59c43e3588135589ace15e9b741643ee2
cogcamp <codeofgenius.camp@gmail.com>
2023-08-11 06:36:38 +0000

    Rina_5

--- Rina_5/main.js
+++ Rina_5/main.js
@@ -2,32 +2,61 @@ var mainScene = new Phaser.Scene("mainScene");
 
 mainScene.create = function() {
     // 初期設定を実行する
-    
+    this.config();
     
     // ボール作成
-    
+    this.createBall();
     
     // パドル作成
-    
+    this.createPaddle();  
     
     // スペースキーのクリックでボール発射
+    this.input.keyboard.on('keydown-SPACE',function(event){
+        //ゲーム開始状態ならば
+        if(this.paddle.isStart){
+            //ボール発射
+            this.ball.setVelocity(this.ballSpeedX,this.ballSpeedY);
+            this.paddle.isStart=false;
+        }
+    },this);
     
     
     // ブロック作成
+    this.createBlocks();
     
     
     // ライフのテキスト表示
-    
+    this.lifeText = this.add.text(40,20,'ライフ:'+this.life,{
+        font:'20px Open Sans',
+        fill:'#ff0000'
+    });
 };
 
 mainScene.update = function() {
     // ボールがシーンの最下部に到達した
+    if(this.ball.y >= this.game.config.height - this.ball.width / 2){
+        this.failToHit();
+    }
     
     
     // キーボードのカーソルオブジェクトを取得
     var cursors = this.input.keyboard.createCursorKeys();
     var x = 0;
-    
+    //右カーソルをクリックすると
+    if(cursors.right.isDown){
+        x=this.paddle.x + this.paddleSpeed;
+        this.paddle.x = Phaser.Math.Clamp(x,52,748);
+    }
+    //左カーソルをクリックすると
+    if(cursors.left.isDown) {
+        x=this.paddle.x-this.paddleSpeed;
+        this.paddle.x = Phaser.Math.Clamp(x,52,748);
+    }
+    //パドルの上にボールが乗っているなら
+    if(this.paddle.isStart){
+        this.ball.setPosition(this.paddle.x,500);
+    }
+
 };
 
 mainScene.config = function() {
@@ -35,40 +64,96 @@ mainScene.config = function() {
     this.cameras.main.setBackgroundColor('#cccccc');
     
     // パドルの移動速度
-    this.paddleSpeed = 10;
+    this.paddleSpeed = 15;
     
     // ボール発射の加速度
     this.ballSpeedX = 0;
-    this.ballSpeedY = -300;
+    this.ballSpeedY = -350;
     
     // ライフ
-    this.life = 3;
+    this.life = 5;
 };
 
 mainScene.createBall = function() {
     // ボール作成
-    
+    this.ball = this.physics.add.image(400,500,'ball1');
+    this.ball.setDisplaySize(22,22)
+    this.ball.setCollideWorldBounds(true);
+    this.ball.setBounce(1);
 };
 
 mainScene.createPaddle = function() {
-     // パドル作成
-    
+     // パドル作成s
+     this.paddle = this.physics.add.image(400,550,'paddle1');
+     this.paddle.setImmovable();
+     this.paddle.isStart = true;
+     this.physics.add.collider(this.paddle,this.ball,this.hitPaddle,null,this);
 };
 
 mainScene.hitPaddle = function (paddle, ball) {
     // ボールにX方向の角度を設定
+    var diff =0;
+    if(ball.x < paddle.x) {
+        //ボールがパドルの左側に衝突
+        diff = paddle.x - ball.x;
+        ball.setVelocityX(-10 * diff);
+    }else{
+        //x方向の加速度はなし
+        ball.setVelocityX(0);
+    }
     
 };
 
 mainScene.createBlocks = function() {
     // 横10列、縦6行並べる
+    //ブロックの色の配列
+    
+    
+    //物理エンジン対象固定オブジェクトグループ作成
+    this.blocks = this.physics.add.staticGroup();
+    
+    //縦に６行
+    for(var i =1; i<=6; i++){
+        
+        
+        
+        //横に10列
+        for( var j = 0;j < 10;j++) {
+            if((i==3||i==4) &&(j==4||j==5)){
+               var color = 'red1'
+           }else if ((i%2==1 && j%2==0)||(i%2==0 && j%2==1)){
+            var color = 'silver1'
+           }else {
+               var color ='blue1'
+           }
+          
+            
+            var block = this.blocks.create(80 + j *64,80 +i *32, color);
+            block.setOrigin(0,0);
+            block.setDisplaySize(64,32);
+            block.refreshBody();
+            if(color=='red1'){
+                block.hp=3
+            }else if(color=='silver1'){
+                block.hp=2
+            }else{
+                block.hp=1
+            }
+        }
+    }
+    
+    this.physics.add.collider(this.ball,this.blocks,this.hitBlock,null,this);
     
     
 };
 
 mainScene.hitBlock = function (ball, block) {
     // 衝突したブロックを削除
-    block.destroy();
+    
+    block.hp--
+    if(block.hp == 0 ){
+        block.destroy();
+    }
     // ブロックの残りを判定
     if (this.blocks.countActive() == 0) {
         // ブロックがなくなると、0.5秒後にゲームクリア
